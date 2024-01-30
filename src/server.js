aaf6d87d7abb12d3dce3621b5ebf1c8e28fc948c
Steven Tea <91791407+Ouistitive@users.noreply.github.com>
2024-01-30 18:28:35 +0100

    regle tout sur le server.js

--- src/server.js
+++ src/server.js
@@ -1,3 +1,14 @@
-import { getData } from "./api.js"
+import Fastify from 'fastify'
+import fastifyStatic from "@fastify/static";
+import path from "path"
+import {fileURLToPath} from 'url'
 
-console.log(await getData("https://gateway.marvel.com:443/v1/public/characters"))
+const app = Fastify()
+app.register(fastifyStatic, {
+    root: path.join(__dirname, 'templates')
+})
+app.get('/', (req, res) => {
+    res.sendFile('index.hbs')
+})
+
+app.listen({ port: 3000 })
