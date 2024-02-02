c4d26a69af2af2601ce255f9578f5c6b2b55eccc
Ouistitive <91791407+Ouistitive@users.noreply.github.com>
2024-02-02 18:02:38 +0100

    Etape 4

--- src/server.js
+++ src/server.js
@@ -1,14 +1,44 @@
-import Fastify from 'fastify'
+import Fastify from 'fastify';
 import fastifyStatic from "@fastify/static";
-import path from "path"
-import {fileURLToPath} from 'url'
+import path from "path";
+import fastifyView from '@fastify/view';
+import handlebars from 'handlebars';
+import {getData} from "./api.js";
 
-const app = Fastify()
+const __dirname = path.resolve();
+
+const app = Fastify();
+
+app.register(fastifyView, {
+    engine: {
+        handlebars: handlebars
+    },
+    templates: 'templates',
+    options: {
+        partials: {
+            header: path.join('header.hbs'),
+            footer: path.join('footer.hbs')
+        }
+    }
+});
+
 app.register(fastifyStatic, {
     root: path.join(__dirname, 'templates')
-})
-app.get('/', (req, res) => {
-    res.sendFile('index.hbs')
-})
+});
 
-app.listen({ port: 3000 })
+app.get('/', (req, res) => {
+
+    res.headers({'Content-Type': 'text/html'});
+
+    getData("https://gateway.marvel.com:443/v1/public/characters")
+        .then(data => {
+            res.view("index.hbs", { data });
+        })
+        .catch(err => {
+            console.error("Erreur lors de la récupération des données :", err);
+            res.status(500).send("Erreur lors de la récupération des données.");
+        });
+
+});
+
+app.listen({ port: 3000, host: '0.0.0.0' });
